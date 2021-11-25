078b4f7f52570c9988eabc7bd930b7b90b8cfccb
shubham_cpp <shubhampawar3007@gmail.com>
2021-11-25 19:00:16 +0530

    make material bundle size smaller

---
+++ client/config-overrides.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+/* config-overrides.js */
+const { useBabelRc, override } = require("customize-cra");
+
+module.exports = override(useBabelRc());
