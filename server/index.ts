fba4e1cad5ef49414a59f70d6d30e3a2d7b28312
shubham_cpp <shubhampawar3007@gmail.com>
2021-11-26 10:49:20 +0530

    cors and page not found

--- server/index.ts
+++ server/index.ts
@@ -9,18 +9,37 @@ const PORT = process.env.PORT || 4001;
 const DB_URI = process.env.DB_URI || '';
 
 // Body parsing Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get(
-  '/',
-  async (_, res: Response): Promise<Response> =>
-    res.status(200).send({
-      message: 'Hello World!',
-    })
-);
+app.use((_, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+// app.get(
+//   '/',
+//   async (_, res: Response): Promise<Response> =>
+//     res.status(200).send({
+//       message: 'Hello World!',
+//     })
+// );
 app.use('/api/admissions', require('./src/routes/admission'));
+
+// page not found
+app.get('/*', (_, res) => {
+  res.status(404).send('404 page not found');
+});
+
 connect(DB_URI).then(() => {
   console.log('Connected to database');
   try {
