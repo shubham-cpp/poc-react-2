91850858514471e902fca5a9ea50d579307b1c3c
shubham_cpp <shubhampawar3007@gmail.com>
2021-11-26 14:51:56 +0530

    causing problems with testing

--- client/config-overrides.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-/* config-overrides.js */
-const { useBabelRc, override } = require("customize-cra");
-
-module.exports = override(useBabelRc());
