9bee3471fe2900553710334b166e8bc7cce197bf
shubham_cpp <shubhampawar3007@gmail.com>
2021-12-03 09:44:10 +0530

    new import style so it covers less lines in app.tsx

---
+++ client/src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as About } from "./About";
+export { default as Admission } from "./Admission";
+export { default as Contact } from "./Contact";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as Home } from "./Home";
+export { default as Images } from "./Images";
