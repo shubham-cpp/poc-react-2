ccb6cd3b8232607aa09811f7bc5b24b6f9595eb9
shubham_cpp <shubhampawar3007@gmail.com>
2021-12-03 09:42:19 +0530

    a better footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,64 +1,44 @@
+import { Box, Typography } from "@mui/material";
 import { ReactElement } from "react";
 import {
   AiFillFacebook,
   AiFillInstagram,
   AiFillTwitterSquare,
 } from "react-icons/ai";
-import { makeStyles } from "@mui/styles";
-import { Theme } from "@mui/material";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  footer: {
-    // borderTop: "2px solid burlywood",
-    position: "absolute",
-    bottom: 5,
-    display: "inline-flex",
-    [theme.breakpoints.down("sm")]: {
-      display: "block",
-    },
-  },
-  socials: {
-    // borderTop: "2px solid burlywood",
-    position: "absolute",
-    bottom: 5,
-    right: 0,
-    fontSize: theme.spacing(2.5),
-    marginLeft: theme.spacing(2),
-    cursor: "pointer",
-    [theme.breakpoints.down("sm")]: {
-      display: "block",
-      bottom: 0,
-      borderTop: "none",
-    },
-    "& span": {
-      padding: theme.spacing(1),
-      "&:hover": {
-        fontSize: theme.spacing(3),
-        background: "linear-gradient(to right,#f12711,#f5af19)",
-      },
-    },
-  },
-}));
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      Learning Curve Public School {"  "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const Footer = (): ReactElement => {
-  const classes = useStyles();
   return (
-    <>
-      <footer className={classes.footer}>
-        Copyright &copy; 2020 Learning Curve Public School
-      </footer>
-      <div className={classes.socials}>
+    <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+      <Typography
+        variant="subtitle1"
+        align="center"
+        color="text.secondary"
+        component="p"
+        gutterBottom
+      >
         <span>
-          <AiFillFacebook />
+          <AiFillFacebook />{" "}
         </span>
         <span>
-          <AiFillInstagram />
+          <AiFillInstagram />{" "}
         </span>
         <span>
-          <AiFillTwitterSquare />
+          <AiFillTwitterSquare />{" "}
         </span>
-      </div>
-    </>
+      </Typography>
+      <Copyright />
+    </Box>
   );
 };
 
