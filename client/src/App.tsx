c7c00c76287d9309ba6e693c7314d6261d14261a
Shubham Pawar <shubhampawar3007@gmail.com>
2021-12-12 19:59:03 +0530

    bg animation and scroll to top btn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ReactElement } from "react";
+import { BsFillCaretUpSquareFill } from "react-icons/bs";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {
   About,
@@ -11,12 +12,27 @@ import {
   Images,
 } from "./components";
 
+const ScrollTopBtn = () => {
+  const toTop = () => {
+    window.scrollTo(0, 0);
+  };
+  return (
+    <button className=" scroll-btn d-block d-sm-none" onClick={toTop}>
+      <BsFillCaretUpSquareFill />
+    </button>
+  );
+};
+
 function App(): ReactElement {
   return (
     <>
       <BrowserRouter>
         <Header />
         <h1 className="text-center heading">Learning Curve Public School</h1>
+
+        <div className="bg"></div>
+        <div className="bg bg2"></div>
+        <div className="bg bg3"></div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
@@ -25,6 +41,7 @@ function App(): ReactElement {
           <Route path="/images" element={<Images />} />
         </Routes>
       </BrowserRouter>
+      <ScrollTopBtn />
       <Footer />
     </>
   );
